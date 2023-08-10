17e0a2282126d81fc80338a45945b6a73f222b66
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-08-10 18:18:10 -0300

    correção do erro na fase 7

--- lib/components/custom_hitbox.dart
+++ lib/components/custom_hitbox.dart
@@ -4,10 +4,10 @@ class CustomHitbox {
   final double width;
   final double height;
 
-  CustomHitbox(
-      {required this.offsetX,
-      required this.offsetY,
-      required this.width,
-      required this.height,
-      ss});
+  CustomHitbox({
+    required this.offsetX,
+    required this.offsetY,
+    required this.width,
+    required this.height,
+  });
 }
